ac5072a2d89396ea3da2ad353992984d30d3d67b
ZhuGH333 <yzhupingn@gmail.com>
2023-02-20 18:08:46 +0100

    Ejercicio 3 Medio hecho

--- app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
+++ app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package dadm.zyang.practica1.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dadm.zyang.practica1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = binding.fragmentContainer.getFragment<NavHostFragment>().navController
+        binding.bnNavigation.setupWithNavController(navController)
     }
 }
