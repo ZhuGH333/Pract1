a3a0604918599de7aa78bdd1e053eec12cfd4a18
ZhuGH333 <yzhupingn@gmail.com>
2023-02-21 16:17:51 +0100

    Ejercicio 4

--- app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
+++ app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
@@ -3,7 +3,10 @@ package dadm.zyang.practica1.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import dadm.zyang.practica1.R
 import dadm.zyang.practica1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         val navController = binding.fragmentContainer.getFragment<NavHostFragment>().navController
         binding.bnNavigation.setupWithNavController(navController)
+
+        setSupportActionBar(binding.idMaterialTool)
+        var appBarConfig = AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.favouritesFragment, R.id.settingsFragments)  )
+        setupActionBarWithNavController(navController, appBarConfig)
+
+
     }
 }
