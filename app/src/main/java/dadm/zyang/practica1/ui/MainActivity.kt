55467b5f8c3a8add925fb1ae880c05e5193db890
ZhuGH333 <yzhupingn@gmail.com>
2023-02-27 16:20:59 +0100

    Ejercicio 6 y 7

--- app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
+++ app/src/main/java/dadm/zyang/practica1/ui/MainActivity.kt
@@ -2,6 +2,11 @@ package dadm.zyang.practica1.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,7 +15,7 @@ import com.google.android.material.navigation.NavigationBarView
 import dadm.zyang.practica1.R
 import dadm.zyang.practica1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,8 +30,20 @@ class MainActivity : AppCompatActivity() {
         var appBarConfig = AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.favouritesFragment, R.id.settingsFragments)  )
         setupActionBarWithNavController(navController, appBarConfig)
 
+        addMenuProvider(this)
+    }
 
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
 
-
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.aboutDialogFragment -> {
+                findNavController(R.id.fragmentContainer).navigate(R.id.aboutDialogFragment )
+                true
+            }
+            else -> false
+        }
     }
 }
